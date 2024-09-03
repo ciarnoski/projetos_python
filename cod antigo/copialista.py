11579de8153660aadcc46129c20a05195a54b1c1
ciarnoski <aciarnoski@gmail.com>
2024-09-03 15:17:36 -0300

    primeiro commit

---
+++ cod antigo/copialista.py
@@ -0,0 +1,7 @@
+lista1 = ["João", "Moto", "Beterraba", "Alicate"]
+
+lista_copia = lista1.copy()
+
+lista1.pop(-1)
+print (lista1)
+print (lista_copia)
---
+++ cod antigo/moradores.py
@@ -0,0 +1,6 @@
+lista1 = ["Roberto", "Anastasia", "Gilmar", "Felicia"]
+residentes_novos = lista1.copy()
+
+residentes_novos.append("Nathanael")
+print(lista1)
+print(residentes_novos)
